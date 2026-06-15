9a90c2a9474cb8300e51f62a416472be1f13f3b7
TAPATEL303DEVTEAM <koshel0400@gmail.com>
2026-06-15 20:59:40 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,36 @@
+const CACHE_NAME = 'tapatel-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './tapatel-logo.png'
+];
+
+// Установка воркера и кэширование файлов
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Активация и удаление старого кэша
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Обработка запросов (работа в офлайн-режиме)
 self.addEventListener('fetch', (event) => {
-  // Браузер видит этот обработчик и считает приложение полноценным PWA
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
